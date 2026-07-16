3039a3683aaa6540f062e1f908189ee6cc63b3b2
Bang-2108 <zoanthibang@gmail.com>
2026-07-16 17:30:21 +0700

    feat: update app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {" "}
-      <body>{children}</body>{" "}
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   );
 }
